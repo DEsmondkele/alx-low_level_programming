3db34dbade24afa22ebdae4f0ee85e95c2e976a5
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-09 20:41:14 -0700

    print combo fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - print double digit combos
- *
- * Description: print double digits combos
- *
- * Return: always 0 success
- */
+* main - print double digit combos
+*
+* Description: print double digit combos
+*
+* Return: always 0 success
+*/
 
 int main(void)
 {
 	int i, j;
-	
+
 	i = 48;
 	j = 48;
-	
-	while(i < 58)
+
+	while (i < 58)
 	{
-		j = 1 + 1;
-		while(j < 58)
+		j = i + 1;
+		while (j < 58)
 		{
 			putchar(i);
 			putchar(j);
-
-			if(i < 56 || j < 57)
-			{
+			if (i < 56 || j < 57)													{
 				putchar(44);
 				putchar(32);
 			}
 			j++;
 		}
-
 		i++;
 	}
-	
+
 	putchar(10);
 
 	return (0);
