1ed117a07fb0a5cf926ab827672e1adcb28e8896
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-13 13:52:12 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 	char tmp[9] = "_putchar";
 	int j = 0;
 
-	for (j = 0; j < 8; J++)
+	for (j = 0; j < 8; j++)
 	{
 		_putchar(tmp[j]);
 	}
-	_putchar('\n);
+	_putchar('\n');
 
 	return(0);
 }
