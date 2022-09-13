8d47c6d71fdcbc4182a7b2cabbe79cd92bd3f494
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-13 15:22:29 -0700

    check for an alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - the program checks for alphabet chracters
+ * @alphabet: alphabet is an ascii characters
+ *
+ * Return: 1 if is alphabet character
+ */
+
+int _isalphabet(int alphabet)
+{
+	if(alphabet >= 'a' && alphabet <= 'z')
+	{
+		return (1);
+	}
+	else if (alphabet >= 'A' && alphabet <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+	 return (0);
+	}
+}
