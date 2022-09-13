0543baf870a619081f60601e2c1d7b68e4e52611
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-13 14:06:34 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,18 @@
 /**
  * main - prints _putchar
  * Description: prints a string "_putchar"
- * Return: Always 0 (Success)
+ * Return: Alwayys 0 (success)
  */
 int main(void)
-{	
+{
 	char tmp[9] = "_putchar";
 	int j = 0;
 
 	for (j = 0; j < 8; j++)
 	{
-		_putchar(tmp[j]);
+		_putchar (tmp[j]);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 
-	return(0);
+	return (0);
 }
