9de238a3cd483083a828c0ece749ec422a8b57b3
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-13 15:49:02 -0700

    c my alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  * Return: 1 if is alphabet character
  */
 
-int _isalpha(int alphabet)
+int _isalpha (int alphabet)
 {
-	if(alphabet >= 'a' && alphabet <= 'z')
+	if (alphabet >= 'a' && alphabet <= 'z')
 	{
 		return (1);
 	}
