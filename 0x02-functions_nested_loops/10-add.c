2924d869da36af95fe4d83287c5d628e1ec8ead7
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-13 18:01:58 -0700

    sum of a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers
+ * @number1: int parameter 1
+ * @number2: int parameter 2
+ *
+ * Description: given 2 integers as arguments and return the sum
+ *
+ * Return: sum of 2 numbers
+ */
+
+int add(int number1, int number2)
+{
+	int sum;
+
+	sum = number1 + number2;
+
+	return (sum);
+}
