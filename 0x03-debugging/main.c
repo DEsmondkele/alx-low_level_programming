37601a6fdc701971bee5d6fab11856d4e1e76581
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-15 17:41:58 -0700

    create my main file

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -2,14 +2,15 @@
 
 /**
  * main - use to test a programs that print positive or negative integers
- * Return: 0 always
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
