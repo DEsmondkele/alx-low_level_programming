6263fa39e24603db92a6e864271d48865e189cb8
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-09-15 17:36:42 -0700

    create my main file

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+#Debugging
+0.main.h
+1.main.c
+2.largest_number.c
+3.print_remaining_days.c
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - use to test a programs that print positive or negative integers
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
